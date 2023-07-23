9808240bbbcabe9ad453c3cbfed11ca4db6b44ed
Artyom <132067879+TyomBen@users.noreply.github.com>
2023-07-23 20:54:31 +0400

    very little change

--- index.php
+++ index.php
@@ -2,39 +2,39 @@
 
 //1
 
-class Calc {
-    public $firstNum;
-    public $secundNum;
+// class Calc {
+//     public $firstNum;
+//     public $secundNum;
 
-    function __construct($firstNum, $secundNum)
-    {   
-        $this -> firstNum = $firstNum;
-        $this -> secundNum = $secundNum;
-    }
-    function plus () {
-        return $this -> firstNum + $this -> secundNum;
-    }
-    function minus () {
-        return $this -> firstNum - $this -> secundNum;
-    }
-    function multiply () {
-        return $this -> firstNum * $this -> secundNum;
-    }
-    function divide () {
-        return $this -> firstNum / $this -> secundNum;
-    }
-}
-$firstNum = new Calc(10, 5);
-echo 'Plus = ' . $firstNum -> plus () . '<br>';
-echo 'Minus = ' . $firstNum -> minus () . '<br>';
-echo 'Multiply = ' . $firstNum -> multiply () . '<br>';
-echo 'Divide = ' . $firstNum -> divide () .'<br><br>';
+//     function __construct($firstNum, $secundNum)
+//     {   
+//         $this -> firstNum = $firstNum;
+//         $this -> secundNum = $secundNum;
+//     }
+//     function plus () {
+//         return $this -> firstNum + $this -> secundNum;
+//     }
+//     function minus () {
+//         return $this -> firstNum - $this -> secundNum;
+//     }
+//     function multiply () {
+//         return $this -> firstNum * $this -> secundNum;
+//     }
+//     function divide () {
+//         return $this -> firstNum / $this -> secundNum;
+//     }
+// }
+// $firstNum = new Calc(10, 5);
+// echo 'Plus = ' . $firstNum -> plus () . '<br>';
+// echo 'Minus = ' . $firstNum -> minus () . '<br>';
+// echo 'Multiply = ' . $firstNum -> multiply () . '<br>';
+// echo 'Divide = ' . $firstNum -> divide () .'<br><br>';
 
-//2
+// //2
 
 class Circle {
     public const PI = 3.14159;
-    public $circle = 4;
+    public $circle;
     public function __construct($circle)
     {
         $this -> circle = $circle;
