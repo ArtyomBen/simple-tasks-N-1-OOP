a2e492b67cd35de992d04bb6f1f33d70d2733f09
Artyom <132067879+TyomBen@users.noreply.github.com>
2023-07-23 20:57:21 +0400

    change

--- index.php
+++ index.php
@@ -2,35 +2,35 @@
 
 //1
 
-// class Calc {
-//     public $firstNum;
-//     public $secundNum;
+class Calc {
+    public $firstNum;
+    public $secundNum;
 
-//     function __construct($firstNum, $secundNum)
-//     {   
-//         $this -> firstNum = $firstNum;
-//         $this -> secundNum = $secundNum;
-//     }
-//     function plus () {
-//         return $this -> firstNum + $this -> secundNum;
-//     }
-//     function minus () {
-//         return $this -> firstNum - $this -> secundNum;
-//     }
-//     function multiply () {
-//         return $this -> firstNum * $this -> secundNum;
-//     }
-//     function divide () {
-//         return $this -> firstNum / $this -> secundNum;
-//     }
-// }
-// $firstNum = new Calc(10, 5);
-// echo 'Plus = ' . $firstNum -> plus () . '<br>';
-// echo 'Minus = ' . $firstNum -> minus () . '<br>';
-// echo 'Multiply = ' . $firstNum -> multiply () . '<br>';
-// echo 'Divide = ' . $firstNum -> divide () .'<br><br>';
+    function __construct($firstNum, $secundNum)
+    {   
+        $this -> firstNum = $firstNum;
+        $this -> secundNum = $secundNum;
+    }
+    function plus () {
+        return $this -> firstNum + $this -> secundNum;
+    }
+    function minus () {
+        return $this -> firstNum - $this -> secundNum;
+    }
+    function multiply () {
+        return $this -> firstNum * $this -> secundNum;
+    }
+    function divide () {
+        return $this -> firstNum / $this -> secundNum;
+    }
+}
+$firstNum = new Calc(10, 5);
+echo 'Plus = ' . $firstNum -> plus () . '<br>';
+echo 'Minus = ' . $firstNum -> minus () . '<br>';
+echo 'Multiply = ' . $firstNum -> multiply () . '<br>';
+echo 'Divide = ' . $firstNum -> divide () .'<br><br>';
 
-// //2
+ //2
 
 class Circle {
     public const PI = 3.14159;
