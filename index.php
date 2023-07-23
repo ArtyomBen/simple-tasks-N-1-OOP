2123111f42a7e798c26b4e2fa98944374f567ee0
Artyom <132067879+TyomBen@users.noreply.github.com>
2023-07-23 20:49:51 +0400

    very little changes

--- index.php
+++ index.php
@@ -28,28 +28,27 @@ $firstNum = new Calc(10, 5);
 echo 'Plus = ' . $firstNum -> plus () . '<br>';
 echo 'Minus = ' . $firstNum -> minus () . '<br>';
 echo 'Multiply = ' . $firstNum -> multiply () . '<br>';
-echo 'Divide = ' . $firstNum -> divide () .'<br>';
-
+echo 'Divide = ' . $firstNum -> divide () .'<br><br>';
 
 //2
 
-// class Circle {
-//     public const PI = 3.14159;
-//     public $circle = 4;
-//     public function __construct($circle)
-//     {
-//         $this -> circle = $circle;
-//     }
+class Circle {
+    public const PI = 3.14159;
+    public $circle = 4;
+    public function __construct($circle)
+    {
+        $this -> circle = $circle;
+    }
 
-//     public function circumference () {
-//         return $this -> circle * 2 * self:: PI;
-//     }
-//     public function area () {
-//         $new_circle = $this -> circle;
-//         return $this -> circle = pow($new_circle, 2) * self:: PI;
-//     }
-// }
+    public function circumference () {
+        return $this -> circle * 2 * self:: PI;
+    }
+    public function area () {
+        $new_circle = $this -> circle;
+        return $this -> circle = pow($new_circle, 2) * self:: PI;
+    }
+}
 
-// $circle = new Circle(4);
-// echo 'Circumference: ' . $circle -> circumference () . '<br>';  // 25.13274
-// echo 'Area: ' . $circle -> area (); // 50.26548
+$circle = new Circle(4);
+echo 'Circumference= ' . $circle -> circumference () . '<br>';  // 25.13274
+echo 'Area= ' . $circle -> area (); // 50.26548
